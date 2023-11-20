cc2767a994ef1abedbd9e97613832d1ea62e60e5
Hoo <adobephotoshop@naver.com>
2023-11-20 21:37:36 +0900

    hello

---
+++ NetCore.Web/Controllers/Membership.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.Web.Models;
+
+namespace NetCore.Web.Controllers
+{
+    public class Membership : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginInfo login)
+        {
+            string message = string.Empty;
+
+            if (ModelState.IsValid)
+            {
+                string id= "junhoo";
+                string pass = "123456";
+
+                if (login.UserId.Equals(id) && login.PassWord.Equals(pass))
+                {
+                    TempData["Message"] = "로그인이 성공적으로 이루어졌습니다.";
+                    return RedirectToAction("Index", "Membership");
+                }
+                else
+                {
+                    message = "로그인되지 않았습니다.";
+                }
+            }
+            else
+            {
+                message = "로그인 정보를 올바르게 입력하세요.";
+            }
+
+            ModelState.AddModelError(string.Empty, message);
+            return View();
+        }
+    }
+}
